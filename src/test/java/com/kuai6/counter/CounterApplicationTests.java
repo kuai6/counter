16c33cbb2e9937ce9eb1b26dc13fe5820cc260be
Aleksei I. Kuleshov <kuai6@ya.ru>
2020-06-19 13:13:58 +0300

    add app

---
+++ src/test/java/com/kuai6/counter/CounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuai6.counter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CounterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
